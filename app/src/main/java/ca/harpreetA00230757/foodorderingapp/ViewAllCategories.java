9aa2b3ce5c7c7b5cdefc5aa3283e62a340b87713
A00230757 <84326658+A00230757@users.noreply.github.com>
2022-02-09 15:36:09 -0500

    useer can search food items based on particular cuisine or just by typing the keyword and get automatic suggestions.

---
+++ app/src/main/java/ca/harpreetA00230757/foodorderingapp/ViewAllCategories.java
@@ -0,0 +1,129 @@
+package ca.harpreetA00230757.foodorderingapp;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ViewAllCategories extends AppCompatActivity {
+    GridView gridView_category;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference mainref;
+
+    ArrayList<CategoryDetails> al;
+    myadapter myad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_categories);
+        gridView_category = findViewById(R.id.gridView_category);
+
+        al = new ArrayList<>();
+        myad = new myadapter();
+        gridView_category.setAdapter(myad);
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mainref = firebaseDatabase.getReference("category");
+        mainref.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    al.clear();
+
+                    for (DataSnapshot single_data_snapshot : snapshot.getChildren()) {
+                        CategoryDetails category_obj = single_data_snapshot.getValue(CategoryDetails.class);
+                        al.add(category_obj);
+                    }
+                    // Toast.makeText(ManageCategory.this,"size of list"+al.size(),Toast.LENGTH_LONG).show();
+
+                    //send refresh to list
+                    myad.notifyDataSetChanged();
+                }
+                else
+                {
+                    al.clear();
+                    myad.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    ////////Adapter code/////
+    class myadapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return al.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return al.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i * 100;
+        }
+
+        @Override
+        public View getView(int i, View customView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            customView = inflater.inflate(R.layout.singlerow_design_of_gridview_category, parent, false);
+
+            CategoryDetails obj = al.get(i);
+
+            TextView tv111 = customView.findViewById(R.id.tv111);
+
+            ImageView imv111 = customView.findViewById(R.id.imv111);
+
+            LinearLayout lv1 = customView.findViewById(R.id.lv1);
+
+            tv111.setText(obj.getCategory_name());
+
+            Picasso.get().load(obj.getPhoto()).resize(500,600).into(imv111);
+            lv1.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Intent in = new Intent(ViewAllCategories.this,ViewProducts.class);
+                    in.putExtra("category",obj.getCategory_name()+"");
+                    startActivity(in);
+                }
+            });
+            return customView;
+
+        }
+    }
+}
