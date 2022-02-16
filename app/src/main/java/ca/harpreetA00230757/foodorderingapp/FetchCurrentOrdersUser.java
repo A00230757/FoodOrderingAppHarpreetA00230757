3f98de4469c797205a06fcef703565781c5b4a6e
A00230757 <84326658+A00230757@users.noreply.github.com>
2022-02-16 18:51:45 -0500

    now user can also see all previous placed orders or current orders.

---
+++ app/src/main/java/ca/harpreetA00230757/foodorderingapp/FetchCurrentOrdersUser.java
@@ -0,0 +1,14 @@
+package ca.harpreetA00230757.foodorderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FetchCurrentOrdersUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch_current_orders_user);
+    }
+}
---
+++ app/src/main/java/ca/harpreetA00230757/foodorderingapp/ViewActiveOrdersUser.java
@@ -0,0 +1,14 @@
+package ca.harpreetA00230757.foodorderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewActiveOrdersUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_active_orders_user);
+    }
+}
